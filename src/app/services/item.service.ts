b8110f36d42a996da70b4d4c1abff5bf714d54cd
MelindaMob <melindamobiye0202@gmail.com>
2025-11-30 15:36:59 +0100

    url

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -3,11 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Item } from '../modele/item.model';
 
+// 1. IMPORT IMPORTANT : Le chemin peut varier selon votre structure de dossiers
+// Si vous êtes dans src/app/services, il faudra peut-être remonter de deux niveaux (../..)
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
-  private apiUrl = 'http://localhost:3000/api/items';
+
+  // 2. REMPLACEMENT : On utilise la variable définie dans environment.ts
+  // On utilise les backticks (`) pour insérer la variable proprement
+  private apiUrl = `${environment.apiUrl}/items`;
 
   constructor(private http: HttpClient) { }
 
